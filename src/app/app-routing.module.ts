eb11b42fd967ca02d17ad3396bfe01b8b49ded12
lorsch <lorenz.schlueter@edu.fhdw.de>
2020-12-01 19:37:12 +0100

    ueber-uns Seite hinzugeführt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { UeberUnsComponent } from './pages/ueber-uns/ueber-uns.component';
 
 const routes: Routes = [
-  {path: '', component: LandingpageComponent}
+  {path: '', component: LandingpageComponent, pathMatch: 'full'},
+  {path: 'ueber-uns', component: UeberUnsComponent}
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,32 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { UeberUnsComponent } from './pages/ueber-uns/ueber-uns.component';
+import { from } from 'rxjs';
+import { ImageViewComponent } from './core/components/image-view/image-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    UeberUnsComponent,
+    ImageViewComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule, // Routing der Anwendung
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
